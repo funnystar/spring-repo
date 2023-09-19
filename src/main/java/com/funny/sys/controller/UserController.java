bb1031333cdea01b3d5bd8811580c3eaeb80456f
funnystar <993675939@qq.com>
2023-09-19 18:14:06 +0800

    第四次提交

--- src/main/java/com/funny/sys/controller/UserController.java
+++ src/main/java/com/funny/sys/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.funny.sys.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.funny.common.vo.Result;
 import com.funny.sys.entity.User;
 import com.funny.sys.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -56,5 +60,25 @@ public class UserController {
         userService.logout(token);
         return Result.success();
 
+    }
+
+    @GetMapping("/list")
+    public Result<Map<String,Object>> getUserList(@RequestParam(value = "username",required = false) String username,
+                                              @RequestParam(value = "phone",required = false) String phone,
+                                              @RequestParam(value = "pageNo") Long pageNo,
+                                              @RequestParam(value = "pageSize") Long pageSize){
+
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(StringUtils.hasLength(username),User::getUsername,username);
+        wrapper.eq(StringUtils.hasLength(phone),User::getPhone,phone);
+
+        Page<User> page = new Page<>(pageNo,pageSize);
+        userService.page(page,wrapper);
+
+        Map<String,Object> data = new HashMap<>();
+        data.put("total",page.getTotal());
+        data.put("rows",page.getRecords());
+
+        return Result.success(data);
     }
 }
